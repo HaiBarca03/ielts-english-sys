d16524ce83ac89a92856ac486f25c9c4281917f5
HaiBarca03 <doanduchai8d2m@gmail.com>
2025-04-14 23:34:04 +0700

    fake api res

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World' })
+})
+
 app.use((err, req, res, next) => {
   res.status(500).json({ error: err.message })
 })
