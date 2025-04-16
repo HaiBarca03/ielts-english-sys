7d4b3580ccf35461df614bcea9c4fee92a40ad06
HaiBarca03 <doanduchai8d2m@gmail.com>
2025-04-16 13:33:03 +0700

    fix-cors

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ const swaggerSetup = require('./src/config/swagger.config')
 // app
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  })
+)
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
